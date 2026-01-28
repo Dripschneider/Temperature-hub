89ab40a72416296529132dcb814fe48bd9bf8bf0
dripper <che.paev@list.ru>
2026-01-28 16:48:31 +0300

    yeah boi

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Input } from './input/input.js';
 import { Card } from './Card/card.js';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setCitiesList] = useState(['New York', 'Kyiv', 'London']); // useState возвращает массив из 2 элеменов, где первый элемент это значение по умолчанию, а второй это функция для изменения. При вызове этой функции тригерит рендер.
   return (
     <div className="Main">
-      <Input />
+      <Input setCitiesList={setCitiesList}/>
       <div className='CardList'>
-        <Card />
-        <Card />
-        <Card />
       </div>
     </div>
   );
--- src/input/input.js
+++ src/input/input.js
@@ -1,10 +1,21 @@
 import '../App.css';
 import React from 'react';
 
-export const Input = () => (
-    <div className="SearchBar">
-        <input className='Input'></input>
-        <button className='Button'>Add</button>
-
-    </div>
-);
+// Функция которая выдает html контейнер с строкой ввода и кнопкой
+export const Input = ({setCitiesList}) => {
+    const handleOnChange = (event) => {
+        console.log('event', event);
+        console.log('hendleOnChange');
+        console.log('value is', event.target.value);
+    };
+    const handleOnClick = () => {
+        setCitiesList(['New York']);
+        console.log('handleOnClick');
+    };
+    return (
+        <div className="SearchBar">
+            <input className='Input'></input>
+            <button className='Button' onClick={handleOnClick}>Add</button>
+        </div>
+    )
+};
